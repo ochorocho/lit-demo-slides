267d5bbedb1f1cf41f14a7248384533cf8a40584
Jochen <jochen.roth@b13.com>
2026-04-06 13:51:55 +0200

    Add base for GitHub pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/lit-demo-slides/',
   // Serve src/ files as static assets so data-markdown can fetch them
   publicDir: false,
   server: {
